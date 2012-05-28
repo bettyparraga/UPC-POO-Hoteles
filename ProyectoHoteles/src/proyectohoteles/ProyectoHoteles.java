be578b4b454a5b128f60ad21a55b9f879a0da009
Jorge Cruz <jcruzac@hotmail.com>
2012-05-28 18:12:34 -0500

    proyecto inicial

---
+++ ProyectoHoteles/src/proyectohoteles/ProyectoHoteles.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectohoteles;
+
+/**
+ *
+ * @author Jorge Cruz
+ */
+public class ProyectoHoteles {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
