c75321a412732bed0261e56697b6465a212a5ecb
Betty Parraga <bparraga@hotmail.com>
2012-05-28 18:44:27 -0500

    mensaje de prueba 1

--- src/proyecto/Proyecto.java
+++ src/proyecto/Proyecto.java
@@ -14,6 +14,6 @@ public class Proyecto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("MEnsaje de prueba 1");
     }
 }
