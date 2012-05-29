1901da1801407c90f29108b2bd1b551c89652d58
Betty Parraga <bparraga@hotmail.com>
2012-05-28 19:23:27 -0500

    mensaje prueba 3

--- ProyectoHoteles/src/proyectohoteles/ProyectoHoteles.java
+++ ProyectoHoteles/src/proyectohoteles/ProyectoHoteles.java
@@ -14,6 +14,7 @@ public class ProyectoHoteles {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("MEnsaje de prueba 3");
+
     }
 }
